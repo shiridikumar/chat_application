d6c295ef0df7ee6e45fa666e93de9c84420874e7
shiridikumar <p.shiridi@research.iiit.ac.in>
2023-04-22 16:50:22 +0530

    updated double ticks

--- server/server.py
+++ server/server.py
@@ -81,23 +81,35 @@ def after_request(response):
 
 def find_key(sid):
     for i in connection_objects:
+        # print(i,connection_objects[i])
         if(connection_objects[i]==sid):
             return i
 
 
-def recv_msg(m,email):
+def recv_msg(m,email,ind):
     global redirection_server,connection_objects,current_outgoing_conns
-    print(connection_objects,"****************")
+    #(connection_objects,"****************")
     
     if(email in connection_objects):
-        print("already present in connection objects")
+        #("already present in connection objects")
         socketio.emit("message",{"from":m["from"],"to":m["to"],"msg":m["msg"],"time":m["time"],"seen":0},room=connection_objects[email])
+        if("from_server" in m):
+            requests.post("http://{}/send_deliver".format(m["from_server"]),data=json.dumps({"from":m["to"],"to":m["from"],"ind":ind}))
+        else:
+            print("************* in connection objects still")
+            chatname=sorted([m["to"],m["from"]])
+            his=db.chats.find_one({"chatname":chatname})
+            chathis=his["history"]
+            chathis[ind]["seen"]=1
+            db.users.find_one_and_update({"chatname":chatname},{"$set":{"history":chathis}})
+            socketio.emit("delivered",{"from":m["to"],"chat_ind":[ind]},room=connection_objects[m["from"]])
+
 
     else:
-        # print(m,"*******************************"
+        # #(m,"*******************************"
         r = requests.post(url=DB_URL, data=json.dumps(m))
         data = r.json()
-        print("***********************",data,"__________________________")
+        #("***********************",data,"__________________________")
 
         if(data["server"]!=server_name):
             ob={"data":m,"from_server":server_name}
@@ -112,22 +124,26 @@ def recv_msg(m,email):
 @socketio.on('disconnect')
 @cross_origin(supports_credentials=True,origin='*')
 def handle_disconnect():
-    global updated_chats
+    global updated_chats,connection_objects
     email=find_key(request.sid)
     res=db.chats.find({"chatname": { "$in": [email] } },{"_id":0})
-    print(updated_chats)
+    #(updated_chats)
     upds=[]
     for  i in res:
         if tuple(i["chatname"]) in updated_chats:
-            # print(i["chatname"],"*************************************")
+            # #(i["chatname"],"*************************************")
             upds.append(i)
     data={"updated":upds}
-    # ls=str(datetime.datetime.now())
-    # print(ls)
-    # d=datetime.datetime.strptime(ls,"%Y-%m-%dT%H:%M:%S.000Z")
-    # print(d)
+    print(email,"_____________________")
+    if(email in connection_objects):
+        print("succesfully removed")
+        connection_objects.pop(email)
+        print(connection_objects)
+
+        
     db.users.find_one_and_update({"email":email},{"$set":{"lastseen":str(datetime.datetime.now())}})
-    print(len(upds),"***************")
+    
+    #(len(upds),"***************")
     r = requests.post(url=CENTRAL_SERVER+str("/backup_data"),data=json.dumps(data))
     print(request.sid,"_______________","Disconnected")
     return {200:2000}
@@ -137,7 +153,6 @@ def handle_disconnect():
 @cross_origin(supports_credentials=True,origin='*')
 def handle_message(data):
     global updated_chats
-    recv_msg(data,data["to"])
     from_=data["from"]
     to=data["to"]
     data["seen"]=0
@@ -145,12 +160,17 @@ def handle_message(data):
     chatname=sorted([from_,to])
     chathis=db.chats.find_one({"chatname":chatname})
     updated_chats[tuple(chatname)]=1
+    ind=0
     if(chathis!=None):
         chathis["history"].append(data)
+        ind=len(chathis["history"])-1
         db.chats.update_one({"chatname":chatname},{ "$set": { 'history': chathis["history"] } })
     else:
         chathis={"history":[data],"chatname":chatname}
         db.chats.insert_one(chathis)
+        ind=len(chathis["history"])-1
+
+    recv_msg(data,data["to"],ind)
     
     # socketio.emit("message",chathis,room=conn)
 
@@ -180,6 +200,7 @@ def handle_connect(data):
                 chathis[j]["seen"]=1
             j-=1
         db.chats.find_one_and_update({"chatname":i["chatname"]},{"$set":{"history":chathis}})
+        updated_chats[tuple(i["chatname"])]=1
         
         
 
@@ -188,7 +209,7 @@ def handle_connect(data):
 
         
 
-    # print(updated_chats)
+    # #(updated_chats)
 
 
     return {"200":"2000"}
@@ -200,19 +221,29 @@ def update_ticks():
     data=json.loads(request.data)
     target=data["from"]
     data=data["updates"]
-    print(data,"****************************************************************")
+    #(data,"****************************************************************")
     for i in data:
         email=i
         if i in connection_objects:
             socketio.emit("delivered",{"from":target,"chat_ind":data[i]},room=connection_objects[i])
     return {200:200}
     
+@app.route("/send_deliver",methods=["POST"])
+@cross_origin(supports_credentials=True,origin='*')
+def send_deliver():
+    global connection_objects
+    data=json.loads(request.data)
+    target=data["from"]
+    chatname=sorted([data["frrom"],data["to"]])
+    ob=db.chats.find_one({"chatname":chatname})
+    his=ob["history"]
+    his[data["ind"]]["seen"]=1
+    db.chats.find_one_and_update({"chatname":chatname},{"$set":{"history":his}})
+    updated_chats[tuple(chatname)]=1
+    if(data["to"] in connection_objects):
+        socketio.emit("delivered",{"from":target,"chat_ind":[data["ind"]]},room=connection_objects[data["to"]])
+    return {200:200}
     
-
-
-
-
-
 
 
 
@@ -226,7 +257,7 @@ def fetchchat():
     chats=db.chats.find_one({"chatname":chatname})
     lastseen=None
     target=db.users.find_one({"email":chat})
-    print(target)
+    #(target)
     if(target):
         lastseen=target["lastseen"]
     if(chats==None):
@@ -240,21 +271,25 @@ def send_from_server():
     global updated_chats
     data=json.loads(request.data)
     from_server=data["from_server"].split(":")[0]
-    print(data,"+++++++++++++++++++++++++++")
+    #(data,"+++++++++++++++++++++++++++")
     data=data["data"]
-    recv_msg(data,data["to"])
     if(from_server!=HOST):
         from_=data["from"]
         to=data["to"]
         chatname=sorted([from_,to])
         chathis=db.chats.find_one({"chatname":chatname})
         updated_chats[tuple(chatname)]=1
+        ind=0
         if(chathis!=None):
             chathis["history"].append(data)
+            ind=len(chathis["history"])-1
             db.chats.update_one({"chatname":chatname},{ "$set": { 'history': chathis["history"] } })
         else:
             chathis={"history":[data],"chatname":chatname}
+            ind=len(chathis["history"])-1
             db.chats.insert_one(chathis)
+    
+    recv_msg(data,data["to"],ind)
         
     return {2000:2000}
 
@@ -263,7 +298,7 @@ def send_from_server():
 @cross_origin(supports_credentials=True,origin='*')
 def userdata():
     data=json.loads(request.data);
-    print(request.data);
+    #(request.data);
     email=data["email"]
     res=db.chats.find({"chatname": { "$in": [email] } })
     contacts=[]
@@ -275,7 +310,7 @@ def userdata():
         if(cont!=""):
             contacts.append(cont)
             lastmessage.append(i["history"][-1]["msg"])
-    print(lastmessage,contacts) 
+    #(lastmessage,contacts) 
     # lastmessage.reverse()
     # contacts.rev
 
